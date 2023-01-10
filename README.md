c7c6a20e118949153e3021eddfd21b61be7c216b
Gavin King <gavin.king@cgi.com>
2023-01-10 23:36:08 +0100

    Load image, make all the pixels grayscale

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # canvas-pixel-manipulation
+
+Another experiment/tutorial. This one can be found at https://www.youtube.com/watch?v=alRBeUMMvDM
+
+Load an image, push the pixels around.
--- src/index.js
+++ src/index.js
@@ -10,5 +10,25 @@ const image1 = new Image();
 image1.src = './pheasant.jpg';
 
 image1.addEventListener('load', () => {
-  ctx.drawImage(image1, 0, 0);
+  ctx.drawImage(image1, 0, -200);
+
+  const scannedImage =
+    ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+  const pixels = scannedImage.data;
+
+  for (let i = 0; i < pixels.length; i += 4) {
+    const total =
+      pixels[i] +
+      pixels[i + 1] +
+      pixels[i + 2];
+
+    const avgColor = total / 3;
+
+    pixels[i] = pixels[i + 1] = pixels[i + 2] = avgColor;
+  }
+
+  scannedImage.data = pixels;
+  ctx.putImageData(scannedImage, 0, 0);
 });
+
